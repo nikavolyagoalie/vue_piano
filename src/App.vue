cdf6f9a933b74e2b1a3a5a600d72b2ed0db2b946
applicant14 <applicant14@clout.one>
2022-05-16 18:21:06 +0300

    audios dont work

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <Piano/>
+  </div>
+</template>
+
+<script>
+import Piano from "@/components/Piano.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Piano
+  },
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
